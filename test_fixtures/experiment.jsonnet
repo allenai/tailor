7c513446a8c97c13653cf86edde5b3ded2e332dd
Akshita Bhagia <akshita23bhagia@gmail.com>
2021-09-29 14:46:52 -0700

    simple addition

---
+++ test_fixtures/experiment.jsonnet
@@ -0,0 +1,5 @@
+{
+    "steps": {
+        "perturb": {"type": "simple-perturbation", "inputs": ["Hi this is a test", "Sample input text"]}
+    }
+}
---
+++ tests/perturbations/simple_perturbation_test.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+
+from tailor.perturbations.simple_perturbation import SimplePerturbation
+from tailor.common.testing import TailorTestCase
+
+
+class TestPerturbations(TailorTestCase):
+    def test_perturbation(self):
+        step = SimplePerturbation()
+        result = step.run(inputs=["Hi this is a test", "Sample input text"])
+        assert result[0] == "hi this is a test!"
+        assert result[1] == "sample input text!"
+
+    def test_deterministic_experiment(self):
+        cmd = [
+            "tango",
+            "run",
+            str(self.FIXTURES_ROOT / "experiment.jsonnet"),
+            "-i",
+            "tailor.perturbations.simple_perturbation",
+            "-d",
+            str(self.TEST_DIR),
+        ]
+        result = subprocess.run(cmd)
+        assert result.returncode == 0
+        assert len(os.listdir(self.TEST_DIR / "step_cache")) == 1
+
+        # Running again shouldn't create any more directories.
+        result = subprocess.run(cmd)
+        assert result.returncode == 0
+        assert len(os.listdir(self.TEST_DIR / "step_cache")) == 1
