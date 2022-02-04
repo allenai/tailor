95bc54cdf2ce1d46abb7a67697c3434f98529d9e
Akshita Bhagia <akshita23bhagia@gmail.com>
2022-02-04 23:09:27 +0530

    docs for convert_dataset_to_dict

--- tailor/steps/convert_dataset_to_dict.py
+++ tailor/steps/convert_dataset_to_dict.py
@@ -7,7 +7,11 @@ from tango.step import Step
 @Step.register("convert-dataset-to-dict")
 class ConvertDatasetToDict(Step):
     """
-    TODO
+    This step converts a Huggingface dataset to dict, and optionally returns an subset.
+
+    .. tip::
+
+        Registered as a :class:`~tango.step.Step` under the name "convert-dataset-to-dict".
     """
 
     DETERMINISTIC = True
@@ -19,6 +23,25 @@ class ConvertDatasetToDict(Step):
         start_idx: Optional[int] = None,
         end_idx: Optional[int] = None,
     ) -> Dict[str, List[Any]]:
+        """
+        Parameters
+        ----------
+
+        dataset : :class:`datasets.Dataset`
+            The huggingface dataset to convert.
+        start_idx : :class:`int`, optional
+            The start index for returning a subset of the data.
+            Default is None.
+        end_idx : :class:`int`, optional
+            The end index for returning a subset of the data.
+            Default is None.
+
+        Returns
+        -------
+
+        :class:`Dict[str, List[Any]]`
+            The dataset in dict format.
+        """
 
         # keys = dataset.features
         data_dict = dataset[start_idx:end_idx]
@@ -37,6 +60,13 @@ class ConvertDatasetToDict(Step):
 
 @Step.register("convert-dict-to-dataset")
 class ConvertDictToDataset(Step):
+    """
+    This step converts a dict to a Huggingface dataset.
+
+    .. tip::
+
+        Registered as a :class:`~tango.step.Step` under the name "convert-dict-to-dataset".
+    """
 
     DETERMINISTIC = True
     CACHEABLE = True
